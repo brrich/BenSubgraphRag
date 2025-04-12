05237c2075c1f19c6962795428f6ca596670684c
Ben Rich <brich4419@gmail.com>
2025-04-12 13:01:49 -0400

    run train uncertain

--- SubGraphRag/retrieve/run_inference_uncertain.sh
+++ SubGraphRag/retrieve/run_inference_uncertain.sh
@@ -3,7 +3,7 @@
 # Slurm sbatch options
 #SBATCH -n 20
 #SBATCH --gres=gpu:volta:1
-#SBATCH -o webqsp_train.sh.log-%j
+#SBATCH -o myScript.sh.log-%j
 
 module load anaconda/2023a-pytorch
 source activate retriever
@@ -13,6 +13,5 @@ echo "starting experiment"
 export WANDB_API_KEY="ba2696a6f8ac298d5721f772e8a8e434da8675bc"
 WANDB_MODE=offline
 
-# python train_uncertain.py -d webqsp
-python train_uncertain.py -d webqsp --mc_samples 100 --num_epochs 50
+python inference_uncertain.py -p trained_models/webqsp_apr12_uncertain_v0/webqsp_Apr12-01:16:56/cpt.pth
 echo "done"
