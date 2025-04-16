04a5cf3c9ce61123064f8dbfd9bb3d8973afc340
Ben Rich <brich4419@gmail.com>
2025-04-15 23:45:26 -0400

    add bnn good

--- SubGraphRag/retrieve/run_eval_uncertain_bnn.sh
+++ SubGraphRag/retrieve/run_eval_uncertain_bnn.sh
@@ -13,5 +13,6 @@ echo "starting experiment"
 export WANDB_API_KEY="b1cf013fa15f74b678f33b1c935f969a8fef57ae"
 WANDB_MODE=offline
 
-python eval_uncertain_bnn.py -d webqsp -p trained_models/webqsp_Apr14-03_bnn_att_1/retrieval_result_bnn_mc20.pth
+python eval_uncertain_bnn.py -d webqsp -p webqsp_Apr15-21:21:51/retrieval_result_bnn_mc100.pth
+
 echo "done"
--- SubGraphRag/retrieve/run_inference_uncertain_bnn.sh
+++ SubGraphRag/retrieve/run_inference_uncertain_bnn.sh
@@ -3,7 +3,7 @@
 # Slurm sbatch options
 #SBATCH -n 20
 #SBATCH --gres=gpu:volta:1
-#SBATCH -o myScript.sh.log-%j
+#SBATCH -o INFERENCE_SCRIPT_LOG.sh.log-%j
 
 module load anaconda/2023a-pytorch
 source activate retriever
@@ -13,5 +13,5 @@ echo "starting experiment"
 export WANDB_API_KEY="ba2696a6f8ac298d5721f772e8a8e434da8675bc"
 WANDB_MODE=offline
 
-python inference_uncertain_bnn.py -p trained_models/webqsp_Apr14_bnn_att_2/cpt.pth
+python inference_uncertain_bnn.py -p webqsp_Apr15-21:21:51/cpt.pth
 echo "done"
