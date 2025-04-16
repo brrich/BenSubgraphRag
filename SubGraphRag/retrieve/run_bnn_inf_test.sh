54fb33365b3c74d318589656481a749a1fe05a0a
Ben Rich <brich4419@gmail.com>
2025-04-16 02:21:35 -0400

    add sh file

--- SubGraphRag/retrieve/run_bnn_inf_test.sh
+++ SubGraphRag/retrieve/run_bnn_inf_test.sh
@@ -11,6 +11,6 @@ source activate retriever
 echo "starting experiment"
 
 # Run BNN inference test script
-python bnn_inf_test.py --model_path /path/to/model_checkpoint.pth --sample_id 0 --mc_samples 10 --top_k 10
+python bnn_inf_test.py --model_path trained_models/webqsp_Apr15_bnn3_working/cpt.pth --sample_id 0 --mc_samples 10 --top_k 10
 
 echo "experiment completed"
